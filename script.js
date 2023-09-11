450490aed8084bb8c8d44befd08588e3220f2e83
Abdullah Suleiman <111006528+Artprentice@users.noreply.github.com>
2023-09-11 15:30:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+//select all images
+const images = document.querySelectorAll('.images img');
 
+//adding scroll event to page
+window.addEventListener('scroll', () => {
+  /*
+  move the images upwards by adding scroll amount
+  and converting it to a negative value
+  */
+  /*
+  We will divide by a bigger number to make the effect
+  subtle.
+  This makes farther object move slower and the closer
+  ones move faster.
+  */
+  images[0].style.top = "-" + (window.scrollY / 2.5) + "px";
+  images[1].style.top = "-" + (window.scrollY / 3.5) + "px";
+  images[2].style.top = "-" + (window.scrollY / 4) + "px";
+  images[3].style.top = "-" + (window.scrollY / 5) + "px";
+  images[4].style.top = "-" + (window.scrollY / 6) + "px";
+  images[5].style.top = "-" + (window.scrollY / 7) + "px";
+  images[6].style.top = "-" + (window.scrollY / 9) + "px";
+});
