5f76268a83c7e5491d61fbaafc705ab00c0704aa
Abdullah Suleiman <111006528+Artprentice@users.noreply.github.com>
2023-09-18 12:03:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,19 @@ window.addEventListener('scroll', () => {
   images[5].style.top = "-" + (window.scrollY / 7) + "px";
   images[6].style.top = "-" + (window.scrollY / 9) + "px";
 });
+ var swiper = new Swiper(".mySwiper", {
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 300,
+        modifier: 1,
+        slideShadows: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    });
