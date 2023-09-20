4d5674fad148943bc6dd997bb89cf3535bf69ca1
abdullah <abdallahsuleiman56@gmail.com>
2023-09-20 20:36:14 +0100

    minor corrections

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # ICT
 This is a site required by Michael. The inner circle trader.  Who posted an online contest as to who can meet his requirements 
 The requirements:
+
+Home page with a dynamic nightscape of a city... motion graphics preferred.
+
+No logo please.
+No image of me.
+
+The wording I have on my present homepage is a good starting point for now...
+
+I only need 3 pages apart from the homepage...
+
+1)  Present Lecture
+Where I can share something publicly... in chart and text.
+
+2)  Your Stories
+Where I make public you as my student & the accomplishments you achieved studying under me.
+
+3)  ICT Compendium
+My YouTube playlists
+
+While i was pressed for time, so far ive got the homepage down( somewhat ).
--- script.js
+++ script.js
@@ -1,18 +1,11 @@
-//select all images
+// Select all images
 const images = document.querySelectorAll('.images img');
 
-//adding scroll event to page
+// Adding scroll event listener to the page
 window.addEventListener('scroll', () => {
-  /*
-  move the images upwards by adding scroll amount
-  and converting it to a negative value
-  */
-  /*
-  We will divide by a bigger number to make the effect
-  subtle.
-  This makes farther object move slower and the closer
-  ones move faster.
-  */
+  // Move the images upwards by adding scroll amount and converting it to a negative value
+  // We will divide by a bigger number to make the effect subtle.
+  // This makes farther objects move slower and the closer ones move faster.
   images[0].style.top = "-" + (window.scrollY / 2.5) + "px";
   images[1].style.top = "-" + (window.scrollY / 3.5) + "px";
   images[2].style.top = "-" + (window.scrollY / 4) + "px";
@@ -21,19 +14,21 @@ window.addEventListener('scroll', () => {
   images[5].style.top = "-" + (window.scrollY / 7) + "px";
   images[6].style.top = "-" + (window.scrollY / 9) + "px";
 });
- var swiper = new Swiper(".mySwiper", {
-      effect: "coverflow",
-      grabCursor: true,
-      centeredSlides: true,
-      slidesPerView: "auto",
-      coverflowEffect: {
-        rotate: 0,
-        stretch: 0,
-        depth: 300,
-        modifier: 1,
-        slideShadows: false,
-      },
-      pagination: {
-        el: ".swiper-pagination",
-      },
-    });
+
+// Initialize Swiper for the ".mySwiper" element
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 300,
+    modifier: 1,
+    slideShadows: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
